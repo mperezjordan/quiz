7d518aa1912acf8e862bab3444c3016d574e90ee
mperezjordan <mpjmario@gmail.com>
2023-08-18 05:01:53 -0400

    First commit

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of Peru?",
+		options: ["Cuzco", "Lima", "Trujillo", "Arequipa"],
+		answer: "Lima",
+	},
+	{
+		id: 2,
+		question: "What is the capital of Mexico?",
+		options: ["Guadalajara", "Monterrey", "CDMX", "Veracruz"],
+		answer: "CDMX",
+	},
+	{
+		id: 3,
+		question: "What is the capital of India?",
+		options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+		answer: "Delhi"
+	},
+	{
+		id: 4,
+		question: "What is the capital of Florida?",
+		options: ["Miami", "Tallahassee", "Tampa", "Orlando"],
+		answer: "Tallahassee"
+	},
+	{
+		id: 5,
+		question: "What is capital of Texas?",
+		options: ["Austin", "Houston", "Dallas", "San Antonio"],
+		answer: "Austin"
+	},
+]
+
+export default qBank;
